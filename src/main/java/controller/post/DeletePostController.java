47c7dbe97f71876c6b3ae1b577e966edb3e49a03
Knock <chein1010@naver.com>
2021-11-21 18:27:14 +0900

    20211121 post delete

--- src/main/java/controller/post/DeletePostController.java
+++ src/main/java/controller/post/DeletePostController.java
@@ -1,23 +1,31 @@
 package controller.post;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import controller.Controller;
 import model.service.PostManager;
+import model.service.ReplyManager;
+import model.service.dto.ReplyDTO;
 
 public class DeletePostController implements Controller {
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response)	throws Exception {
+    	PostManager post = PostManager.getInstance();
+    	ReplyManager reply = ReplyManager.getInstance();
     	int postId = 0;
-    	PostManager manager = PostManager.getInstance();
-    	    	
     	try {
     		if(request.getParameter("pid") != null)
     			postId = Integer.parseInt(request.getParameter("pid"));
     		else
     			throw new Exception("Post Id 값이 없습니다!");
-    		manager.delete(postId);
+    		List<ReplyDTO> list = reply.getReplyListByPost(postId);
+    		for(ReplyDTO r : list) {
+    			reply.delete(r.getReplyId());
+    		}
+    		post.delete(postId);
     		return "redirect:/board/list";
     	} catch (Exception e) {
     		request.setAttribute("exception", e);
